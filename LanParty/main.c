b58177274c4c370db0bd11a9bd1c8fa3f3fe64d9
Bogdan <102182293+bogdan624@users.noreply.github.com>
2023-05-19 01:46:45 +0300

    Add files via upload

--- LanParty/main.c
+++ LanParty/main.c
@@ -1,42 +1,44 @@
 #include "tasks.h"
 
-int main()
+int main(int argc,char **argv)
 {
-    FILE *fisier_taskuri, *check;
+    FILE *fisierC,*fisierD,*fisierR;
     int *task;
     TL *head = NULL;
     TL *OptEchipe = NULL;
     int Nr_Echipe = 0;
-    fisier_taskuri=fopen("c.in","rt");
-    check = fopen("test.txt","wt");
+    fisierC=fopen(argv[1],"rt");
+    fisierD=fopen(argv[2],"rt");
+    fisierR = fopen(argv[3],"wt");
     task=allocate_memory(5*sizeof(int));
-    check_null(fisier_taskuri);
-    check_null(check);
-    fscanf(fisier_taskuri,"%d %d %d %d %d",&task[0],&task[1],&task[2],&task[3],&task[4]);
+    check_null(fisierC);
+    check_null(fisierD);
+    check_null(fisierR);
+    fscanf(fisierC,"%d %d %d %d %d",&task[0],&task[1],&task[2],&task[3],&task[4]);
     NAE * arbore = allocate_memory(sizeof(NAE));
     if(task[0]==1){
-        ReadTeamList(&head,&Nr_Echipe);
-        DisplayTeams(head,Nr_Echipe,check);
-        fclose(check);
+        ReadTeamList(&head,&Nr_Echipe,fisierD);
+        DisplayTeams(head,Nr_Echipe,fisierR);
+        fclose(fisierR);
     }
     if(task[1]==1){
-        check = fopen("test.txt","wt");
-        check_null(check);
+        fisierR = fopen(argv[3],"wt");
+        check_null(fisierR);
         RemoveTeams(&head,&Nr_Echipe);
-        DisplayTeams(head,Nr_Echipe,check);
+        DisplayTeams(head,Nr_Echipe,fisierR);
     }
     if(task[2]==1){
-        AfisareTask3(head,Nr_Echipe,check,&OptEchipe);
+        AfisareTask3(head,Nr_Echipe,fisierR,&OptEchipe);
     }
     if(task[3]==1){
-        arbore = AfisareTask4(OptEchipe,arbore,check);
+        arbore = AfisareTask4(OptEchipe,arbore,fisierR);
     }
     if(task[4]==1){
         AVL * ceva = allocate_memory(sizeof(AVL));
         TL* lista_ajutor = NULL;
         transferDataNAEtoTL(arbore,&lista_ajutor);
         reverseTL(&lista_ajutor);
-        AfisareTask5(lista_ajutor,ceva,check);
+        AfisareTask5(lista_ajutor,ceva,fisierR);
     }
     return 0;
 }
